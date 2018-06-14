fe011451ec8cb4237092e9b8e6aeee110473c6b5
Aaron <jieran.peng@ambow.com>
2018-06-14 18:40:13 +0800

    增加security整合的工具

---
+++ spring-boot-spring-security-mybatisplus/src/main/java/com/mtoliv/entity/Account.java
@@ -0,0 +1,62 @@
+package com.mtoliv.entity;
+
+import com.baomidou.mybatisplus.activerecord.Model;
+import com.baomidou.mybatisplus.annotations.TableName;
+import java.io.Serializable;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author Aaron
+ * @since 2018-06-13
+ */
+@TableName("t_account")
+public class Account extends Model<Account> {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String username;
+    private String password;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    protected Serializable pkVal() {
+        return this.id;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+        ", id=" + id +
+        ", username=" + username +
+        ", password=" + password +
+        "}";
+    }
+}
---
+++ spring-boot-spring-security-mybatisplus/src/main/java/com/mtoliv/web/controller/AccountController.java
@@ -0,0 +1,56 @@
+package com.mtoliv.web.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mtoliv.entity.Account;
+import com.mtoliv.service.AccountService;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author Aaron
+ * @since 2018-06-11
+ */
+@RestController
+@RequestMapping("/api/v1/accounts")
+@Api(tags = "账号相关接口")
+public class AccountController {
+
+	@Autowired
+	private AccountService accountService;
+	
+	@Autowired
+	private JedisConnectionFactory jedisConnectionFactory;
+	
+	@PutMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@ApiOperation(value = "新增账号")
+	public Account put(@RequestBody Account account) {
+		
+		accountService.insertOrUpdate(account);
+		System.out.println(account.getId());
+		return account;
+	}
+	
+	@GetMapping
+	@ApiOperation(value = "测试redis是否可用")
+	public void testRedis() {
+		
+		RedisConnection redisConnection = jedisConnectionFactory.getConnection();
+		redisConnection.append("1".getBytes(), "1".getBytes());
+	}
+}
