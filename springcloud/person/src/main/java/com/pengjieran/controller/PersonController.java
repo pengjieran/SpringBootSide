9c07edbbaef67a26fc556a051fdf64afda974675
Aaron <pengjieran@gmail.com>
2018-11-05 13:43:58 +0800

    增加接口实现

---
+++ springcloud/person/src/main/java/com/pengjieran/controller/PersonController.java
@@ -0,0 +1,10 @@
+package com.pengjieran.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pengjieran.service.PersonService;
+
+@RestController
+public class PersonController implements PersonService {
+
+}
