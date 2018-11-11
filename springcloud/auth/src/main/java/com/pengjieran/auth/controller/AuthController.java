d554ed39c3568a3648f32cabcdfa922b8cfc832e
Aaron <pengjieran@gmail.com>
2018-11-11 16:52:26 +0800

    增加认证接口

---
+++ springcloud/auth/src/main/java/com/pengjieran/auth/controller/AuthController.java
@@ -0,0 +1,12 @@
+package com.pengjieran.auth.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/auth/api/v1/auth", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+public class AuthController {
+
+    
+}
