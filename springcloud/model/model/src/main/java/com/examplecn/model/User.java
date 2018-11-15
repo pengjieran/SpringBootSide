7d5623205302aba47180d15bb6b33ff0a5fe419d
Aaron <pengjieran@gmail.com>
2018-11-15 17:36:44 +0800

    增加model

---
+++ springcloud/model/model/src/main/java/com/examplecn/model/User.java
@@ -0,0 +1,12 @@
+package com.examplecn.model;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+	private static final long serialVersionUID = -3035251335348123124L;
+
+	private String username;
+	
+	private String password;
+}
