6137e62ac2f6c106dcb0fc94522ccb9f9c3f96f2
Aaron <pengjieran@gmail.com>
2018-11-21 16:25:06 +0800

    编译问题修正

--- springcloud/person/src/main/java/com/pengjieran/controller/PersonController.java
+++ springcloud/person/src/main/java/com/pengjieran/controller/PersonController.java
@@ -3,7 +3,7 @@ package com.pengjieran.controller;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.pengjieran.model.Person;
+import com.examplecn.model.Person;
 import com.pengjieran.service.PersonService;
 
 @RestController
@@ -12,12 +12,7 @@ public class PersonController implements PersonService {
 	@Override
 	public Person get(@RequestParam(value = "id", required = true) String id) {
 		
-		Person person = new Person();
-		person.setId("123456");
-		person.setName("123456");
-		person.setSex(1);
-		
-		return person;
+		 return Person.builder().age(30).name("123456").six(1).build();
 	}
 
 }
