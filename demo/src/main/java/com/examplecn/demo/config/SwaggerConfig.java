6ffae712330dd38187873e883fd68063e1df1b49
Aaron <pengjieran@gmail.com>
2019-12-30 18:58:47 +0800

    配置更新

--- demo/src/main/java/com/examplecn/demo/config/SwaggerConfig.java
+++ demo/src/main/java/com/examplecn/demo/config/SwaggerConfig.java
@@ -4,13 +4,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -18,12 +24,43 @@ public class SwaggerConfig {
 
     @Bean
     public Docket createAccepterRestApi() {
+
+        List<Parameter> pars = new ArrayList<>();
+
+
+        ParameterBuilder contentType = new ParameterBuilder();
+        contentType.name("Content-Type").defaultValue("application/json")
+                .modelRef(new ModelRef("string")).parameterType("header")
+                .required(true).build();
+
+        ParameterBuilder product = new ParameterBuilder();
+        product.name("product").defaultValue("product").description("终端类型")
+                .modelRef(new ModelRef("string")).parameterType("header")
+                .required(true).build();
+
+        ParameterBuilder token = new ParameterBuilder();
+        token.name("token").defaultValue("token").description("认证token")
+                .modelRef(new ModelRef("string")).parameterType("header")
+                .required(false).build();
+
+        ParameterBuilder appKey = new ParameterBuilder();
+        appKey.name("appKey").defaultValue("appKey").description("appKey")
+                .modelRef(new ModelRef("string")).parameterType("header")
+                .required(false).build();
+
+
+        pars.add(contentType.build());    //根据每个方法名也知道当前方法在设置什么参数
+        pars.add(product.build());
+        pars.add(token.build());
+        pars.add(appKey.build());
+
         return new Docket(DocumentationType.SWAGGER_2)
                 .groupName("单体应用相关API")//分组名,不指定默认为default
                 .select()
                 .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))// 扫描的包路径
                 .paths(PathSelectors.any())// 定义要生成文档的Api的url路径规则
                 .build()
+                .globalOperationParameters(pars)
                 .apiInfo(apiInfo())// 设置swagger-ui.html页面上的一些元素信息
                 .enable(true);
     }
